61092d61fb76de91a161411deaab592a8ee893ff
Alexander Chernenko <letmeexist95@gmail.com>
2025-07-11 09:35:41 +0300

    Renamed Model dir to ViewModel to represent the architecture better

---
+++ ViewModel/CombinedModel.cpp
@@ -0,0 +1,182 @@
+#include "CombinedModel.hpp"
+
+#include <QStringLiteral>
+
+CombinedModel::CombinedModel(QObject* const parent) noexcept
+    : QAbstractItemModel{ parent }
+{}
+
+QModelIndex CombinedModel::index(int row, int column,
+    const QModelIndex& index) const
+{
+    const auto& countries = m_Storage.GetData();
+
+    if (!index.isValid() && row < countries.size())
+    {
+        return createIndex(row, column, -1);
+    }
+
+    if (index.internalId() == m_CountryIndex
+        && row < countries[index.row()].Operators.size())
+    {
+        return createIndex(row, column, index.row());
+    }
+
+    return {};
+}
+
+QModelIndex CombinedModel::parent(const QModelIndex& index) const
+{
+    if (!index.isValid() || index.internalId() == m_CountryIndex)
+    {
+        return {};
+    }
+
+    return createIndex(index.internalId(), 0, -1);
+}
+
+int CombinedModel::rowCount(const QModelIndex& index) const
+{
+    const auto& countries = m_Storage.GetData();
+
+    if (!index.isValid())
+    {
+        return countries.size();
+    }
+    else if (index.internalId() == m_CountryIndex)
+    {
+        return countries[index.row()].Operators.size();
+    }
+
+    return {};
+}
+
+int CombinedModel::columnCount(const QModelIndex& index) const
+{
+    return 1;
+}
+
+
+QVariant CombinedModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid())
+    {
+        return {};
+    }
+
+    const auto& countries = m_Storage.GetData();
+
+    const int id = index.internalId();
+    const int row = index.row();
+
+    if (id == m_CountryIndex)
+    {
+        const Country& country = countries[row];
+
+        return GetCountryData(country, role);
+    }
+
+    const Country& country = countries[id];
+    const Operator& op = country.Operators[row];
+
+    return GetOperatorData(op, role);
+}
+
+QHash<int, QByteArray> CombinedModel::roleNames() const
+{
+    return
+    {
+        { Role::OperatorName, "operatorName" },
+        { Role::Mnc, "mnc" },
+
+        { Role::CountryName, "countryName" },
+        { Role::CountryCode, "countryCode" },
+        { Role::Mcc, "mcc" },
+
+        { Role::Level, "level" }
+    };
+}
+
+Qt::ItemFlags CombinedModel::flags(const QModelIndex& index) const
+{
+    return ((index.isValid()) ? (Qt::ItemIsEnabled | Qt::ItemIsSelectable)
+        :(Qt::NoItemFlags));
+}
+
+void CombinedModel::AddOperator(const qint32 mcc, const qint32 mnc,
+    const QString& name) noexcept
+{
+
+}
+
+void CombinedModel::UpdateOperator(const QString& name, const qint32 mcc,
+    const qint32 mnc) noexcept
+{
+    const auto searchResult = m_Storage.FindOperator(mcc, mnc);
+
+    if (!searchResult.Result)
+    {
+        return;
+    }
+
+    const QModelIndex changedIndex
+    {
+        index(searchResult.OperatorIndex, 0,
+            index(searchResult.CountryIndex, 0))
+    };
+
+    emit dataChanged(changedIndex, changedIndex, { Role::OperatorName });
+}
+
+void CombinedModel::RemoveOperator(const qint32 mcc, const qint32 mnc) noexcept
+{
+    const auto searchResult = m_Storage.FindOperator(mcc, mnc);
+
+    if (!searchResult.Result)
+    {
+        return;
+    }
+
+    beginRemoveRows(index(searchResult.CountryIndex, 0),
+        searchResult.OperatorIndex, searchResult.OperatorIndex);
+
+    m_Storage.RemoveOperator(searchResult);
+
+    endRemoveRows();
+}
+
+QVariant CombinedModel::GetCountryData(const Country& country, const int role)
+    const noexcept
+{
+    switch (role)
+    {
+        case Role::CountryName:
+            return country.Name;
+        case Role::CountryCode:
+            return country.Code;
+        case Role::Mcc:
+            return country.Mcc;
+        case Role::Level:
+            return 0;
+    }
+
+    return {};
+}
+
+QVariant CombinedModel::GetOperatorData(const Operator& op, const int role)
+    const noexcept
+{
+    switch (role)
+    {
+        case Role::OperatorName:
+            return op.Name;
+        case Role::Mnc:
+            return op.Mnc;
+        case Role::Mcc:
+            return op.Mcc;
+        case Role::Level:
+            return 1;
+    }
+
+    return {};
+}
---
+++ ViewModel/CombinedModel.hpp
@@ -0,0 +1,72 @@
+#ifndef COMBINEDMODEL_HPP
+#define COMBINEDMODEL_HPP
+
+#include <QObject>
+#include <QAbstractItemModel>
+#include <QList>
+#include <QHash>
+
+#include "Data/Storage/DataStorage.hpp"
+
+class CombinedModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+public:
+    enum Role
+    {
+        OperatorName = Qt::UserRole + 1,
+        Mnc,
+
+        CountryName,
+        CountryCode,
+        Mcc,
+
+        Level
+    };
+
+private:
+    struct OperatorSearchResult
+    {
+        qsizetype CountryIndex{};
+        qsizetype OperatorIndex{};
+        bool Result{};
+    };
+
+public:
+    CombinedModel(QObject* const parent = nullptr) noexcept;
+
+    ~CombinedModel() = default;
+
+    QModelIndex index(int row, int column, const QModelIndex& index = {})
+        const override;
+    QModelIndex parent(const QModelIndex& index) const override;
+
+    int rowCount(const QModelIndex& index) const override;
+    int columnCount(const QModelIndex& index) const override;
+
+    QVariant data(const QModelIndex& index, int role) const override;
+
+    QHash<int, QByteArray> roleNames() const override;
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+
+public slots:
+    void AddOperator(const qint32 mcc, const qint32 mnc,
+        const QString& name) noexcept;
+    void UpdateOperator(const QString& name, const qint32 mcc,
+        const qint32 mnc) noexcept;
+    void RemoveOperator(const qint32 mcc, const qint32 mnc) noexcept;
+
+private:
+    QVariant GetCountryData(const Country& country, const int role)
+        const noexcept;
+    QVariant GetOperatorData(const Operator& op, const int role)
+        const noexcept;
+
+private:
+    DataStorage m_Storage{ this };
+
+    const int m_CountryIndex{ -1 };
+};
+
+#endif
